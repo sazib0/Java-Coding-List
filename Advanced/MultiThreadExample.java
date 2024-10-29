b38612770edae49f0cba428488388a620f508412
Sazib <sazibc5@gmail.com>
2024-10-29 07:07:43 +0600

    Standard outputs should not be used directly to log anything (java:S106)

--- Advanced/MultiThreadExample.java
+++ Advanced/MultiThreadExample.java
@@ -1,4 +1,9 @@
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class MultiThreadExample {
+    private static final Logger LOGGER = Logger.getLogger(MultiThreadExample.class.getName());
+
     public static void main(String[] args) {
         // Creating and starting the threads
         Thread thread1 = new Thread(new PrintNumbers(), "Thread-1");
@@ -10,15 +15,16 @@ public class MultiThreadExample {
 }
 
 class PrintNumbers implements Runnable {
+    private static final Logger LOGGER = Logger.getLogger(PrintNumbers.class.getName());
+
     @Override
     public void run() {
         for (int i = 1; i <= 5; i++) {
-            System.out.println(Thread.currentThread().getName() + " - " + i);
+            LOGGER.log(Level.INFO, "{0} - {1}", new Object[]{Thread.currentThread().getName(), i});
             try {
                 Thread.sleep(1000); // Sleep for 1 second
             } catch (InterruptedException e) {
-                // Properly handle the interruption
-                System.err.println(Thread.currentThread().getName() + " was interrupted");
+                LOGGER.log(Level.SEVERE, Thread.currentThread().getName() + " was interrupted", e);
                 Thread.currentThread().interrupt(); // Restore the interrupt status
                 break;
             }
